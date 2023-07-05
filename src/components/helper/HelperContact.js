46a1cb3de63f2c19e241ac195505607eca4596c2
Diksha127 <diksha.appxperts@gmail.com>
2023-07-05 12:44:32 +0530

    helper booking and contact

--- src/components/helper/HelperContact.js
+++ src/components/helper/HelperContact.js
@@ -8,11 +8,176 @@ import { Link } from 'react-router-dom';
 
 const HelperContact = () => {
   const [selectedLink, setSelectedLink] = useState('/');
-
-  // const navigate = useNavigate();
+  const [storedData, setstoreddata] = React.useState([]);
+  const [ishelperloggedin, setishelperloggedin] = React.useState(false);
+  const [jwtToken, setjwtToken] = useState('');
+  const [list, setList] = useState([]);
+  const [typeValue, setTypeValue] = useState("");
+  const [infoValue, setInfoValue] = useState("");  
+  const [contactdata, setcontactdata] = useState({
+    "OrgId": 1,
+    "HelperCode": "",
+    "HomeAddress": "",
+    "HomeTelephone": "",
+    "WhatsApp": "",
+    "Viber": "",
+    "Facebook": "",
+    "OtherContact": [
+      {
+        "OrgId": 1,
+        "HelperCode": "",
+        "Type": "",
+        "Information": ""
+      }
+    ]
+  });
+ 
+  
   useEffect(() => {
     setSelectedLink(window.location.pathname);
+    fetchTokenHandler();
+    let token = localStorage.getItem("helpertoken");
+    console.log(token);
+    if (token) {
+      console.log(token);
+      setishelperloggedin(true);
+      setstoreddata(JSON.parse(token));
+      console.log(storedData);
+    }
+    console.log(ishelperloggedin);
+    console.log(storedData);
   }, []);
+  
+  useEffect(() => {
+    if (storedData.length > 0) {
+      setcontactdata({
+        ...contactdata,
+        "OrgId": 1,
+        "HelperCode": storedData[0].HelperCode,
+        "HomeAddress": storedData[0].HomeAddress,
+        "HomeTelephone": storedData[0].HomeTelephone,
+        "WhatsApp": storedData[0].WhatsApp,
+        "Viber": storedData[0].Viber,
+        "Facebook": storedData[0].Facebook,
+        "OtherContact": [
+          {
+            "OrgId": 1,
+            "HelperCode": storedData[0].HelperCode,
+            "Type": "",
+            "Information": ""
+          }
+        ]
+      });
+    }
+  }, [storedData]);
+
+
+  const fetchTokenHandler = async () => {
+    const tokenDetail = {
+      Username: 'admin',
+      Password: 'admin54321',
+    };
+    try {
+      const response = await fetch('http://154.26.130.251:283/api/Token/GenerateToken', {
+        method: 'POST',
+        body: JSON.stringify(tokenDetail),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) {
+        console.log('Something went wrong!');
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+      console.log(data.Jwt_Token);
+      setjwtToken(data.Jwt_Token);
+    } catch (error) {}
+  };
+
+  const handleAdd = () => {
+    if (typeValue !== "" && infoValue !== "") {
+      setList((prevList) => [...prevList, { Type: typeValue, Information: infoValue ,OrgId:1,HelperCode:storedData[0].HelperCode }]);
+      setTypeValue("");
+      setInfoValue("");
+    }
+  };
+
+  const handleTypeChange = (event) => {
+    setTypeValue(event.target.value);
+  };
+  
+  const handleInfoChange = (event) => {
+    setInfoValue(event.target.value);
+  };
+
+  const savecontactdataHandler = async (event) => {
+    event.preventDefault();
+
+    const regDetail = {
+      "OrgId": 1,
+      "HelperCode": contactdata.HelperCode,
+      "HomeAddress": contactdata.HomeAddress,
+      "HomeTelephone": contactdata.HomeTelephone,
+      "WhatsApp": contactdata.WhatsApp,
+      "Viber": contactdata.Viber,
+      "Facebook": contactdata.Facebook,
+      "OtherContact": [
+        {
+          "OrgId": 1,
+          "HelperCode": contactdata.HelperCode,
+          "Type": "",
+          "Information": ""
+        }
+      ]
+    };
+    console.log(regDetail);
+
+    const jwttoken = jwtToken;
+    console.log(jwtToken, jwttoken);
+
+    try {
+      const response = await fetch('http://154.26.130.251:283/api/Helper/ContactDetails_Updation', {
+        method: 'POST',
+        body: JSON.stringify(regDetail),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwttoken}`,
+        },
+      });
+
+      console.log(response);
+
+      if (!response.ok) {
+        console.log('Something went wrong!');
+        return;
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.Code === 200 && data.Message === 'Sucess') {
+       
+        const updatedData = [...storedData]; // Clone the array
+        updatedData[0] = { ...updatedData[0],
+            "OrgId": 1,
+            "HelperCode": contactdata.HelperCode,
+            "HomeAddress": contactdata.HomeAddress,
+            "HomeTelephone": contactdata.HomeTelephone,
+            "WhatsApp": contactdata.WhatsApp,
+            "Viber": contactdata.Viber,
+            "Facebook": contactdata.Facebook
+          }; // Update the property
+        setstoreddata(updatedData);
+        console.log(storedData,updatedData);
+        
+        localStorage.setItem('helpertoken', JSON.stringify(updatedData));
+      }
+    } catch (error) {
+      console.log('An error occurred:', error);
+    }
+ 
+  };
 
   const handleLinkClick = (link) => {
     //navigate(link);
@@ -64,6 +229,7 @@ const HelperContact = () => {
             </div>
             </div>
             <div className="col-lg-8">
+              <form onSubmit={savecontactdataHandler}>
               <div className="dashboard-right-wrap hct-wrap">
                     <div className="main-inner-box">
                       <div className="pageTitle title-fix sm">
@@ -74,7 +240,10 @@ const HelperContact = () => {
                                 <label>Home Address</label>
                               </div>
                               <div className="col-lg-7">
-                                <textarea className="form-control" placeholder="Postal Code"></textarea>
+                                <textarea className="form-control" placeholder="Home Address"
+                                value={contactdata.HomeAddress}
+                                onChange={(e)=>{setcontactdata({...contactdata,HomeAddress:e.target.value});}}
+                                ></textarea>
                               </div>
                             </div>  
                              <div className="row form-group align-items-center">
@@ -82,7 +251,10 @@ const HelperContact = () => {
                           <label>Home Telephone</label>
                         </div>
                         <div className="col-lg-7">
-                           <input type="text" className="form-control" placeholder="Home Telephone" /> 
+                           <input type="text" className="form-control" placeholder="Home Telephone"
+                           value={contactdata.HomeTelephone}
+                           onChange={(e)=>{setcontactdata({...contactdata,HomeTelephone:e.target.value});}}
+                            /> 
                         </div>
                       </div>   
                        <div className="row form-group align-items-center">
@@ -90,7 +262,10 @@ const HelperContact = () => {
                           <label>Viber</label>
                         </div>
                         <div className="col-lg-7">
-                           <input type="text" className="form-control" placeholder="Viber" /> 
+                           <input type="text" className="form-control" placeholder="Viber"
+                           value={contactdata.Viber}
+                           onChange={(e)=>{setcontactdata({...contactdata,Viber:e.target.value});}}
+                            /> 
                         </div>
                       </div> 
                        <div className="row form-group align-items-center">
@@ -98,7 +273,22 @@ const HelperContact = () => {
                           <label>Facebook</label>
                         </div>
                         <div className="col-lg-7">
-                           <input type="text" className="form-control" placeholder="Facebook" /> 
+                           <input type="text" className="form-control" placeholder="Facebook"
+                           value={contactdata.Facebook}
+                           onChange={(e)=>{setcontactdata({...contactdata,Facebook:e.target.value});}}
+                            /> 
+                        </div>
+                      </div> 
+
+                      <div className="row form-group align-items-center">
+                        <div className="col-lg-5">
+                          <label>WhatsApp</label>
+                        </div>
+                        <div className="col-lg-7">
+                           <input type="text" className="form-control" placeholder="WhatsApp"
+                           value={contactdata.WhatsApp}
+                           onChange={(e)=>{setcontactdata({...contactdata,WhatsApp:e.target.value});}}
+                            /> 
                         </div>
                       </div> 
                             <div className="row mb15 sub-title-row form-group align-items-center">
@@ -111,21 +301,36 @@ const HelperContact = () => {
                           <label>Type</label>
                         </div>
                         <div className="col-lg-7">
-                          <input type="text" className="form-control" placeholder="WeChat"/> </div>
+                          <input type="text" className="form-control" placeholder="Type"
+                          value={typeValue}
+                          onChange={handleTypeChange}
+                          /> </div>
                       </div>
                       <div className="row form-group align-items-center">
                         <div className="col-lg-5">
-                          <label>Contact No</label>
+                          <label>Information</label>
                         </div>
                         <div className="col-lg-7">
-                          <input type="text" className="form-control" placeholder="WeChat Account ID/Name"/> </div>
+                          <input type="text" className="form-control" placeholder="Info"
+                          value={infoValue}
+                          onChange={handleInfoChange}
+                          /> </div>
                       </div>
 
                       <div className="row form-group align-items-center">
                         <div className="col-lg-12">
-                          <button name="add-more" className="add-more"><i className="fas fa-plus"></i> Add More</button>
+                          <button name="add-more" onClick={handleAdd} className="add-more"><i className="fas fa-plus"></i> Add More</button>
                         </div>
                       </div> 
+                      {/* <ul>
+                        {list.map((item, index) => (
+                          <li key={index}>
+                            Type: {item.type}, Info: {item.info}
+                          </li>
+                        ))}
+                      </ul> */}
+
+                     
                       {/* <div className="row mb15 sub-title-row form-group align-items-center">
                               <div className="col-lg-12">
                                 <h6>Next of Kin</h6>
@@ -148,9 +353,10 @@ const HelperContact = () => {
                         
               </div>
               <div className="row mt20 justify-content-end">
-                      <div className="col-auto"><button type="button" className="custom-button">SAVE</button></div>
+                      <div className="col-auto"><button type="submit" className="custom-button">SAVE</button></div>
                     </div>
               </div>
+              </form>
             </div>
           </div>
         </div>
